c7f4bb9229bf6826d552e80860706a786cf1497b
ajithsocs <ajithsocs@4b44807b-f7cf-9d26-f8a1-ab2892027615>
2012-10-15 13:46:05 +0000

    TimeSheet model1

---
+++ src/main/java/org/cssa/iface/gui/timesheet/TimeSheetController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.cssa.iface.gui.timesheet;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author ajith
+ *
+ */
+public class TimeSheetController implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

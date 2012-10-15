b6fc70710baae1d9b83e2b4540533bdd94093233
ajithsocs <ajithsocs@4b44807b-f7cf-9d26-f8a1-ab2892027615>
2012-10-15 13:46:05 +0000

    TimeSheet model1

---
+++ branches/cssa/src/main/java/org/cssa/iface/gui/timesheet/TimeSheetTableModel.java
@@ -0,0 +1,30 @@
+package org.cssa.iface.gui.timesheet;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * 
+ * @author ajith
+ *
+ */
+public class TimeSheetTableModel  extends AbstractTableModel{
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

ecb4df98d432d69ec8d08ed4c676a7596ebf103c
ajithsocs <ajithsocs@4b44807b-f7cf-9d26-f8a1-ab2892027615>
2012-11-25 07:04:55 +0000

    admin bug fix

---
+++ branches/cssa/src/main/java/org/cssa/iface/util/TableScriptXML.java
@@ -0,0 +1,79 @@
+/**
+ * 
+ */
+package org.cssa.iface.util;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.log4j.Logger;
+import org.cssa.iface.bo.TableStore;
+
+/**
+ * @author ajith
+ *
+ */
+public class TableScriptXML {
+	private static final Logger log = Util.getLogger(TableScriptXML.class);
+	private static final String TABLE_DETAILS_XML = "interface_table_script.xml";
+	private TableStore tableStore;
+	private Map<String, String> tableStoreMap;
+	
+	public TableScriptXML() {
+		init();
+	}
+
+	private void init() {
+		try{
+			tableStoreMap = new HashMap<String, String>();
+			JAXBContext context = JAXBContext
+					.newInstance(TableStore.class);
+			Unmarshaller um = context.createUnmarshaller();
+			URL url = this.getClass().getClassLoader().getResource(TABLE_DETAILS_XML);
+			tableStore = (TableStore) um.unmarshal(url);
+			int size = tableStore.getTableDetailsList().size();
+			for(int i = 0; i < size; i++) {
+				tableStoreMap.put(tableStore.getTableDetailsList().get(i).getTableName(), tableStore.getTableDetailsList().get(i).getScriptFile());
+			}
+		} catch (JAXBException e) {
+			log.error(e);
+		}
+	}
+
+	
+	/**
+	 * @return the tableStore
+	 */
+	public TableStore getTableStore() {
+		return tableStore;
+	}
+
+	/**
+	 * @param tableStore the tableStore to set
+	 */
+	public void setTableStore(TableStore tableStore) {
+		this.tableStore = tableStore;
+	}
+
+	/**
+	 * @return the tableStoreMap
+	 */
+	public Map<String, String> getTableStoreMap() {
+		return tableStoreMap;
+	}
+
+	/**
+	 * @param tableStoreMap the tableStoreMap to set
+	 */
+	public void setTableStoreMap(Map<String, String> tableStoreMap) {
+		this.tableStoreMap = tableStoreMap;
+	}
+	
+	
+
+}
